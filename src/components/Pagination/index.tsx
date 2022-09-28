760a046da1db706a32c0245d91b2db7c64dbd7f4
João Debussy <social.debussy@gmail.com>
2022-09-28 20:27:12 -0300

    refactor: responsiveness, link navigation

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -4,12 +4,19 @@ import { PaginationItem } from './PaginationItem';
 
 export function Pagination() {
   return (
-    <Stack direction="row" spacing="6" mt="8" justify="space-between" align="center">
+    <Stack
+      direction={['column',
+      'row']}
+      spacing="6"
+      mt="8"
+      justify="space-between"
+      align="center"
+    >
       <Box>
         <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
       </Box>
       <Stack direction="row" spacing="2">
-        <PaginationItem isCurrent number={1} />
+        <PaginationItem number={1} isCurrent />
         <PaginationItem number={2} />
         <PaginationItem number={3} />
         <PaginationItem number={4} />
