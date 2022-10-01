940db10509dab7930ea77d15daa7771424468060
João Debussy <social.debussy@gmail.com>
2022-10-01 17:53:16 -0300

    feat: creating useUsers custom hook

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,39 +1,19 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue, Spinner } from '@chakra-ui/react';
 import { RiAddLine } from 'react-icons/ri';
-import { useQuery } from 'react-query';
 import Link from 'next/link';
 
-import { api } from '../../services/api';
+import { useUsers } from '../../services/hooks/useUsers';
 
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
 import { Pagination } from '../../components/Pagination';
 
 export default function UserList() {
+  const { data, isLoading, isFetching, error } = useUsers();
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
-  });
-
-  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-    const { data } = await api.get('/users')
-
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month:'long',
-          year: 'numeric',
-        }),
-      };
-    });
-
-    return users;
-  }, {
-    staleTime: 1000 * 5,  // 5 seconds
   });
 
   return (
